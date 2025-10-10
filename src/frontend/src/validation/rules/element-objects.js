daacd7325e8905a33cb5ba2b62a8ccd9bc058991
kay11beg <techkaiya6@gmail.com>
2025-10-10 18:53:51 +0900

    [add] 暫定コミット：フィールド項目におけるバリデーションオブジェクトの追加

---
+++ src/frontend/src/validation/rules/element-objects.js
@@ -0,0 +1,22 @@
+
+/**
+ * バリデーション用オブジェクト
+ **/
+
+const elementObject = {
+    email: {"メールアドレス": ""},
+    password: {"パスワード": ""},
+    themeName: {"学習テーマ名": ""},
+    themeText: {"学習テキスト": ""},
+    isUrlChecked: {"URL情報の入力を許可": false},
+    urlKey1: {"URLキータグ1": ""},
+    urlKey2: {"URLキータグ2": ""},
+    urlKey3: {"URLキータグ3": ""},
+    urlKey4: {"URLキータグ4": ""},
+    urlKey5: {"URLキータグ5": ""},
+    urlValue1: {"URL値1": ""},
+    urlValue2: {"URL値2": ""},
+    urlValue3: {"URL値3": ""},
+    urlValue4: {"URL値4": ""},
+    urlValue5: {"URL値5": ""}
+};
